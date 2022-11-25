1462d150c78e10f42d516316c4e4005bd6a5bc75
Azamat Ozhakayev <ozhakaev@gmail.com>
2022-11-26 01:34:54 +0500

    09 done

---
+++ 09/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// int i = 2;
+// while(i<=n)
+// {
+//     Console.Write($"{i} ");
+//     i=i+2;
+// }
+Console.WriteLine();
+for (int j = 1; j <= n; j++)
+{
+    if (j % 2 == 0) Console.Write($"{j} ");
+}
