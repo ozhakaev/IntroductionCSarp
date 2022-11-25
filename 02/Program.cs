0f1fa27de1ffd58695ed8acb5f8c1a53dcf5c302
Azamat Ozhakayev <ozhakaev@gmail.com>
2022-11-25 21:46:18 +0500

    02 done

---
+++ 02/Program.cs
@@ -0,0 +1,13 @@
+﻿double a,b;
+Console.WriteLine("Введите число a: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+b = Convert.ToDouble(Console.ReadLine());
+
+if ((a*a) == b){
+    Console.WriteLine($"Число {b} является квадратом числа {a}");
+} else if ((b*b) == a){
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+} else {
+    Console.WriteLine("Оба числа не являются кватратом по отношению друг к другу");
+}
